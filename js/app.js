5bd7d3cee122c7a74416730eda80c4c1cc7ace68
taegorov <taegorov@gmail.com>
2021-03-23 14:00:47 -0700

    adds Canvas tag, random product generation

--- js/app.js
+++ js/app.js
@@ -31,23 +31,52 @@ const Product = function (productName, imageSrc) {
     Product.all.push(this);
 }
 
+
 Product.all = [];
 
 function pickNewProduct() {
+
+    // previous product images
+    const previousProductOne = productOne;
+    const previousProductTwo = productTwo;
+    const previousProductThree = productThree;
+
     shuffle(Product.all);
-    productOne = Product.all[0];
-    productTwo = Product.all[1];
-    productThree = Product.all[2];
-  
+    // productOne = Product.all[0];
+    // productTwo = Product.all[1];
+    // productThree = Product.all[2];
+
+    // ensures next product shown is not the same as previous product shown
+    for (let product of Product.all) {
+        if (product !== previousProductOne && product !== previousProductTwo && product !== previousProductThree) {
+            productOne = product;
+        }
+    }
+
+    // ensures next product shown is not the same as previous product, or same as productOne
+    for (let product of Product.all) {
+        if (product !== previousProductOne && product !== previousProductTwo && product !== previousProductThree && product !== productOne) {
+            productTwo = product;
+        }
+    }
+    
+    // ensures next product shown is not the same as previous product, or same as productOne or productTwo
+    for (let product of Product.all) {
+        if (product !== previousProductOne && product !== previousProductTwo && product !== previousProductThree && product !== productOne && product !== productTwo) {
+            productThree = product;
+        }
+    }
+
     renderNewProduct();
 }
+
 
 const renderNewProduct = function () {
 
     productOneImageTag.src = productOne.url;
     productOneImageTag.alt = productOne.productName;
     productOneHeaderTag.textContent = productOne.productName;
-  
+
     productTwoImageTag.src = productTwo.url;
     productTwoImageTag.alt = productTwo.productName;
     productTwoHeaderTag.textContent = productTwo.productName;
@@ -55,7 +84,7 @@ const renderNewProduct = function () {
     productThreeImageTag.src = productThree.url;
     productThreeImageTag.alt = productThree.productName;
     productThreeHeaderTag.textContent = productThree.productName;
-  };
+};
 
 const handleClickOnProduct = function (event) {
     if (totalClicks < maxClicks) {
@@ -66,43 +95,42 @@ const handleClickOnProduct = function (event) {
 
             if (id === 'product-one-img') {
                 productOne.clicks += 1;
-            } else if (id === 'product-two-img'){
+            } else if (id === 'product-two-img') {
                 productTwo.clicks += 1;
             } else if (id === 'product-three-img')
                 productThree.clicks += 1;
         }
 
-            // adds +1 to total times the image/product is shown
-            productOne.timesShown += 1;
-            productTwo.timesShown += 1;
-            productThree.timesShown += 1;
-
-            pickNewProduct();
-}
+        // adds +1 to total times the image/product is shown
+        productOne.timesShown += 1;
+        productTwo.timesShown += 1;
+        productThree.timesShown += 1;
         // +1 to number of totalClicks
         totalClicks += 1;
+        pickNewProduct();
+    }
 
-        // remove click function when maxClicks is reached
-        if (totalClicks === maxClicks) {
-            productSectionTag.removeEventListener('click', handleClickOnProduct);
-            alert('25 products liked, thanks for participating!');
-        
-            //display likes to page after hitting maxClicks
-            renderLikes();
+    // remove click function when maxClicks is reached
+    if (totalClicks === maxClicks) {
+        productSectionTag.removeEventListener('click', handleClickOnProduct);
+        alert('25 products liked, thanks for participating!');
 
-        }
+        //display likes to page after hitting maxClicks
+        renderLikes();
+
+    }
 }
 
 function renderLikes() {
     const likesListElem = document.getElementById('product-clicks');
     likesListElem.innerHTML = '';
-    for (let i = 0; i < Product.all.length; i+= 1) {
-      const productPicture = Product.all[i];
-      const productItemElem = document.createElement('li');
-      likesListElem.appendChild(productItemElem);
+    for (let i = 0; i < Product.all.length; i += 1) {
+        const productPicture = Product.all[i];
+        const productItemElem = document.createElement('li');
+        likesListElem.appendChild(productItemElem);
 
-      // X had Y votes, and was seen Z times.
-      productItemElem.textContent = productPicture.productName + ' had ' + productPicture.clicks + ' votes, and was seen ' + productPicture.timesShown + ' times.';
+        // X had Y votes, and was seen Z times.
+        productItemElem.textContent = productPicture.productName + ' had ' + productPicture.clicks + ' votes, and was seen ' + productPicture.timesShown + ' times.';
     }
 }
 
@@ -110,11 +138,11 @@ function renderLikes() {
 // Fisher yates  shuffle https://medium.com/@nitinpatel_20236/how-to-shuffle-correctly-shuffle-an-array-in-javascript-15ea3f84bfb
 
 function shuffle(array) {
-    for (let i = array.length - 1; i > 0; i-=1) {
-      const j = Math.floor(Math.random() * i)
-      const temp = array[i]
-      array[i] = array[j]
-      array[j] = temp
+    for (let i = array.length - 1; i > 0; i -= 1) {
+        const j = Math.floor(Math.random() * i)
+        const temp = array[i]
+        array[i] = array[j]
+        array[j] = temp
     }
 }
 
@@ -147,7 +175,7 @@ new Product('Wine Glass', 'img/wine-glass.jpg');
 
 pickNewProduct();
 
-console.log ('total clicks are: ', totalClicks);
+console.log('total clicks are: ', totalClicks);
 
 // View results button learned from W3Schools.com
 function viewResults() {
@@ -158,3 +186,22 @@ function viewResults() {
         x.style.display = 'block';
     }
 }
+
+
+// ChartJS 
+
+const productNamesArray = [];
+const productLikesArray = [];
+
+
+// build arrays
+for (let i = 0; i < Product.all.length; i++) {
+    const singleProductName = Product.all[i].name;
+    productNamesArray.push(singleProductName);
+  }
+
+  for (let i = 0; i < Product.all.length; i++) {
+    const singleProductLikes = Product.all[i].clicks;
+    productLikesArray.push(singleProductLikes);
+  }
+
